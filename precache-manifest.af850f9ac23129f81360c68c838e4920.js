7c369fab8a689d3cc39bee05f7e539253db15ab7
Nexhan <subodhkhadka3@gmail.com>
2020-07-03 21:19:53 -0700

    Updates

---
+++ precache-manifest.af850f9ac23129f81360c68c838e4920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3d5f43105c0dcb6428b93a3034dadb",
+    "url": "/Lotto/index.html"
+  },
+  {
+    "revision": "e476c2ad69a561503ba1",
+    "url": "/Lotto/static/css/main.f5f299d3.chunk.css"
+  },
+  {
+    "revision": "d1324b82e429730339dd",
+    "url": "/Lotto/static/js/2.80e542c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lotto/static/js/2.80e542c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e476c2ad69a561503ba1",
+    "url": "/Lotto/static/js/main.9e5b3a5c.chunk.js"
+  },
+  {
+    "revision": "31f2061660e97fb309a7",
+    "url": "/Lotto/static/js/runtime-main.6258c6ec.js"
+  }
+]);
